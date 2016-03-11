224b1bb8fc2a5b4490d35541c681b63780eb8927
qinglinyi <wdmzjwsf@163.com>
2016-03-11 17:08:56 +0800

    update README

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 Support Vector Drawable Demo
 
 这是一个使用Support v7完成VectorDrawable的Demo。
+
+
+# AppCompatImageView
+
+1. ImageView（及其子类） 使用
+
+   * 在布局文件中使用的话 使用app:srcCompat代替android:src
+   * 在代码中使用的话直接使用setImageResource,和以前没有区别
+
+2. 其他地方，如果想在5.0以下使用的话，需要用其他Drawable容器包装以下
--- app/src/main/java/com/qinglinyi/vectordrawable/MainActivity.java
+++ app/src/main/java/com/qinglinyi/vectordrawable/MainActivity.java
@@ -3,6 +3,7 @@ package com.qinglinyi.vectordrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +23,9 @@ public class MainActivity extends AppCompatActivity {
 
         // 2. 其他地方，如果想在5.0以下使用的话，需要用其他Drawable容器包装以下
         // （StateListDrawable, InsetDrawable, LayerDrawable, LevelListDrawable, and RotateDrawable）
+
+        TextView mTextView= (TextView) findViewById(R.id.text_view);
+        mTextView.setBackgroundResource(R.drawable.android_green_ic_wrap);
+
     }
 }
